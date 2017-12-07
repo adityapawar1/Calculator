966d19f8ff6adb14094b9a11c6608f5d66b94b76
Aditya Pawar <aditya.pawar@warriorlife.net>
2017-12-07 14:38:58 -0800

    Added Evaluting and Operators and Numbers

--- calculator.js
+++ calculator.js
@@ -1,1 +1,21 @@
 // JavaScript Document
+//Change Display
+function d(val) {
+	document.getElementById("d").value = va;;
+}
+
+// Type Num and Operators
+
+function v(val) {
+	document.getElementById("d").value +=val;
+}
+//Evaluate the expressions
+
+function e() {
+	try {
+		d(eval(document.getElementbyId("d").value));
+	}
+	catch(err) {
+		d("Error");
+	}
+}
